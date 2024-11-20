274248807b47e0b4efa599659ee6e9ebf22b81a4
DhishonThomas <dhishonthomas23@gmail.com>
2024-11-20 19:12:37 +0530

    Added bcryptjs... added function for password hash in utils

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,18 +1,22 @@
-import { Request,Response } from "express";
-import userSchema from "../models/userModel";
+import { Request, Response } from "express";
+import User from "../models/userModel";
 
-export class userController{
-    constructor(){}
+export class userController {
+  constructor() {}
 
-    async user_login(req:Request,res:Response){
-        console.log("controller reached")
-        res.send("Welcome to login.")
-        }
+  async user_login(req: Request, res: Response) {
+    console.log("controller reached");
+    res.send("Welcome to login.");
+  }
 
-        async user_signUp(req:Request,res:Response){
-            const {name,email,password}=req.body
+  async user_signUp(req: Request, res: Response) {
+    const { name, email, password } = req.body;
 
-            const db= userSchema()
-            res.send("Welcome to signup")
-        }
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+        res.status(200).json({status:false,message:"Email already existing"})
+    }
+    
+    res.send("Welcome to signup");
+  }
 }
