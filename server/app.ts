6af631da444640d517f2508ec63f5b7263e5c498
DhishonThomas <dhishonthomas23@gmail.com>
2024-11-20 18:32:01 +0530

    Import dotenv and connect to mongodb.

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { Request,Response } from 'express'
 import router from './routes/userRoute'
+import db from './config/db'
+import dotenv from 'dotenv'
 
 const app=express()
-
-
+dotenv.config()
+db()
 // app.use(express.json())
 
 app.use("/api",router)
