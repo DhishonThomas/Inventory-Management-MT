fb1ddd5266147dc9702830aa48a7a29c50c285e4
DhishonThomas <dhishonthomas23@gmail.com>
2024-11-21 00:36:11 +0530

    Added the customerRoutes..

--- server/controllers/customerController.ts
+++ server/controllers/customerController.ts
@@ -3,5 +3,21 @@ import { Request,Response } from "express";
 export class customerController{
     constructor(){}
 
+    async getAllCustomers(req:Request,res:Response){
+
+    }
+
+    async createCustomer(req:Request,res:Response){
+
+    }
+
+    async updateCustomer(req:Request,res:Response){
+
+    }
+
+    async deleteCustomer(req:Request,res:Response){
+
+    }
+
 
 }
--- server/routes/customerRoute.ts
+++ server/routes/customerRoute.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import { customerController } from '../controllers/customerController'
+import { authMiddleware } from '../middlewares/userAuthMiddleware'
 const customer_controller=new customerController()
 
 const router = express.Router()
 
-
+router.get("/:userId",authMiddleware,(req,res)=>customer_controller.getAllCustomers(req,res))
+router.post("/",authMiddleware,(req,res)=>customer_controller.createCustomer(req,res))
+router.put("/",authMiddleware,(req,res)=>customer_controller.updateCustomer(req,res))
+router.delete("/",authMiddleware,(req,res)=>customer_controller.deleteCustomer(req,res))
 
 export default router
