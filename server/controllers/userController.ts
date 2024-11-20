defdc8e17c48a0fce65ec4b081d359016b735740
DhishonThomas <dhishonthomas23@gmail.com>
2024-11-20 20:05:09 +0530

    Completed user login code. Added the password method for password validation.

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,14 +1,43 @@
 import { Request, Response } from "express";
 import User from "../models/userModel";
 import { hashedPassword } from "../utils/passwordManager";
+import { generateToken } from "../utils/jwtManager";
 
 export class userController {
   constructor() {}
 
   async user_login(req: Request, res: Response) {
-    console.log("controller reached");
-    res.send("Welcome to login.");
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(200).json({ status: false, message: "Invalid credentials" });
+    }
+
+    const findUser = await User.findOne({ email });
+
+    if (!findUser) {
+      res.status(200).json({ status: false, message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await (User as any).comparePassword(password);
+
+    if (!isPasswordValid) {
+      res.status(200).json({ status: false, message: "Invalid credentials" });
+      return;
+    }
+    const payload = { userId: findUser?._id };
+    const token = generateToken(payload);
+
+    res
+      .status(201)
+      .json({
+        token: token,
+        status: true,
+        message: "Success fully token generated.",
+      });
   }
+
+
 
   async user_signUp(req: Request, res: Response) {
     const { name, email, password } = req.body;
@@ -20,12 +49,10 @@ export class userController {
         .json({ status: false, message: "Email already existing" });
     }
     if (name.length >= 3) {
-      res
-        .status(200)
-        .json({
-          status: false,
-          message: "Name must be more than three letters",
-        });
+      res.status(200).json({
+        status: false,
+        message: "Name must be more than three letters",
+      });
     }
     const hashed_password = await hashedPassword(password);
 
@@ -37,15 +64,14 @@ export class userController {
 
     console.log(db);
     if (!db) {
-       res
+      res
         .status(200)
         .json({ status: false, message: "Server not created user.error" });
-        return
+      return;
     }
 
     res
       .status(200)
       .json({ status: false, message: "Inventory user created successfully." });
   }
-  
 }
--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -5,6 +5,7 @@ export interface IUser{
     name:string;
     email:string;
     password:string;
+    comparePassword(password:string):Promise<boolean>;
 }
 
 
