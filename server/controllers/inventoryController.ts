b8509b08ccd9380e199e1f126247875863249f08
DhishonThomas <dhishonthomas23@gmail.com>
2024-11-21 00:42:46 +0530

    Added inventory Route...etc.

--- server/controllers/inventoryController.ts
+++ server/controllers/inventoryController.ts
@@ -3,5 +3,21 @@ import { Request,Response } from "express";
 export class InventoryController{
     constructor(){}
 
+async getInventors(req:Request,res:Response){
+
+}
+
+async createInventory(req:Request,res:Response){
+
+}
+
+async updateInventory(req:Request,res:Response){
+
+}
+
+async deleteInventory(req:Request,res:Response){
+
+}
+
 
 }
--- server/routes/inventoryRoute.ts
+++ server/routes/inventoryRoute.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { InventoryController } from '../controllers/inventoryController'
+import { authMiddleware } from '../middlewares/userAuthMiddleware'
 const inventory_controller=new InventoryController()
 
 const router =express.Router()
 
-
-
-
+router.get("/:userId",authMiddleware,(req,res)=>inventory_controller.getInventors(req,res))
+router.post("/",authMiddleware,(req,res)=>inventory_controller.createInventory(req,res))
+router.put("/",authMiddleware,(req,res)=>inventory_controller.updateInventory(req,res))
+router.delete("/",authMiddleware,(req,res)=>inventory_controller.deleteInventory(req,res))
 
 export default router
