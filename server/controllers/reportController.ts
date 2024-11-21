83ad35ae99583af32d8275a780837546708a6a97
DhishonThomas <dhishonthomas23@gmail.com>
2024-11-21 18:15:16 +0530

    Updated sales route.

--- server/controllers/reportController.ts
+++ server/controllers/reportController.ts
@@ -1,19 +1,23 @@
 import { Request,Response } from "express";
-
+import Sales from '../models/salesModel'
 export class reportController{
     constructor() {}
 
  async sales(req:Request,res:Response){
 const {userId}=req.body
+if(!userId){
+    res.status(200).json({status:false,message:"No userIds"})
+}
+
 
  }
     
 async inventory(req:Request,res:Response){
 const {userId}=req.body
+
 }
 
 async customer(req:Request,res:Response){
- 
     const {userId,customerId}=req.body
 
 }
--- server/routes/salesRoute.ts
+++ server/routes/salesRoute.ts
@@ -6,6 +6,6 @@ const router = express.Router()
 
 
 router.post("/sales",authMiddleware,(req,res)=>sales_controller.createSale(req,res))
-router.get("/sales",authMiddleware,(req,res)=>sales_controller.getAllSales(req,res))
+router.get("/sales/:userId",authMiddleware,(req,res)=>sales_controller.getAllSales(req,res))
 
 export default router
