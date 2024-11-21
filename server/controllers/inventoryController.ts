c7d3c28d5012277fe07d50f01f63a0bc66b6a51a
DhishonThomas <dhishonthomas23@gmail.com>
2024-11-21 13:40:32 +0530

    Inventory updated controller done.

--- server/controllers/inventoryController.ts
+++ server/controllers/inventoryController.ts
@@ -1,5 +1,6 @@
 import { Request,Response } from "express";
 import Inventory from "../models/inventoryModel"
+import { InventoryValidator } from "../utils/validatorManager";
 export class InventoryController{
     constructor(){}
 
@@ -13,6 +14,28 @@ async createInventory(req:Request,res:Response){
 
 async updateInventory(req:Request,res:Response){
 
+    const {name,description,quantity,price,inventoryId,userId}=req.body
+
+    if(!userId||!inventoryId){
+        res.status(200).json({status:false,message:"NO userIds"})
+    }
+
+    const result = InventoryValidator(name,description,quantity,price)
+
+    if(!result.status){
+        res.status(200).json({status:result.status,message:result.message})
+        return
+    }
+
+    const updateInventory=await Inventory.findByIdAndUpdate({_id:inventoryId,userId:userId},{
+        name:name,description:description,quantity:quantity,price
+    },{new:true})
+
+    if(!updateInventory){
+        res.status(200).json({status:false,message:"Server not update the Inventory"})
+    }
+
+    res.status(201).json({status:true,message:"Successfully updated the Inventory"})
 }
 
 async deleteInventory(req:Request,res:Response){
--- server/utils/validatorManager.ts
+++ server/utils/validatorManager.ts
@@ -19,3 +19,30 @@ export function customerValidator(name: string, address: string, mobile: number)
 
   return { status: true, message: "Every field is validated" };
 }
+
+
+
+export function InventoryValidator(name: string, description: string,quantity:number, price: number) {
+  if (name.length >= 3) {
+    const result = {
+      status: false,
+      message: "Name want to be more that 2 letters",
+    };
+    return result;
+  } else if (description.length >= 200) {
+    const result = {
+      status: false,
+      message: "Description cannot be more that 200 letters or space",
+    };
+    return result;
+  } else if (quantity<0) {
+    const result = { status: false, message: "Quantity want to be more that 0" };
+    return result;
+  } else if (price<0) {
+    const result = { status: false, message: "Price want to be more that 0" };
+    return result;
+  }
+
+  return { status: true, message: "Every field is validated" };
+}
+
