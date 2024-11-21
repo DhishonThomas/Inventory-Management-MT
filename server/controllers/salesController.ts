86142991e0da313a473339d5e2334aa22fd44c9f
DhishonThomas <dhishonthomas23@gmail.com>
2024-11-21 18:21:56 +0530

    Created Sales...done

--- server/controllers/salesController.ts
+++ server/controllers/salesController.ts
@@ -1,11 +1,19 @@
 import { Request,Response } from "express";
-
+import Sales from '../models/salesModel'
 
 export class salesController{
     constructor(){}
 
     async createSale(req:Request,res:Response){
+const {itemId,quantity,customerId,paymentType,userId}=req.body
 
+const createSale=await Sales.create({item_id:itemId,quantity:quantity,customer_id:customerId,payment_type:paymentType,userId:userId})
+
+if(!createSale){
+    res.status(200).json({status:false,message:"Server not created Sale"})
+
+}
+res.status(201).json({status:true,message:"Successfully created sale"})
     }
 
     async getAllSales(req:Request,res:Response){
