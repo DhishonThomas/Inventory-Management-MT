a5be3f833e8838db327e7c4f8e9aac98a54322b8
DhishonThomas <dhishonthomas23@gmail.com>
2024-11-23 18:51:35 +0530

    Added toast in inventory edit

--- client/src/pages/InventoryEdit.tsx
+++ client/src/pages/InventoryEdit.tsx
@@ -4,17 +4,34 @@ import Wrapper from "../components/ui/Wrapper";
 import userApi from "../utils/axiosInterceptors/userApiService";
 import Button from "../components/ui/Button";
 import { validateDescription, validateFullName } from "../utils/validator";
+import { toast } from "react-toastify";
 
 const InventoryEdit = ({ userId, inventoryData }: any) => {
   const [name, setName] = useState(inventoryData.name);
   const [description, setDescription] = useState(inventoryData.description);
-  const [quantity, setQuantity] = useState<number | string>("");
+  const [quantity, setQuantity] = useState<number | string>(inventoryData.quantity);
   const [price, setPrice] = useState(inventoryData.price);
 
   const handleSubmit = async () => {
     const checkName = validateFullName(name);
     const checkDescription = validateDescription(description);
+if(checkName!=null){
+    toast.success(checkName.errMessage, {
+        position: "top-center",
+        autoClose: 5000,
+        theme: "dark",
+      });
+    return 
+}
 
+if(checkDescription!=null){
+    toast.success(checkDescription.errMessage, {
+        position: "top-center",
+        autoClose: 5000,
+        theme: "dark",
+      });
+    return 
+}
     const response = await userApi.put("/inventory/", {
       name,
       description,
